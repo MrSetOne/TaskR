e3443d545b5b54f3848031fbaae95edd01e0c0ff
MrSetOne <lara.sanchez.michael@gmail.com>
2022-05-03 15:57:06 +0200

    fix

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 //Librerias que se van a usar
-const moment = require('moment');
 const readline = require('readline');
 
 //Interfaz de readline
@@ -10,57 +9,57 @@ const rl = readline.createInterface({
 
 //Mascas de si las tareas estan realizadas
 const TaskOK = "[X]"
-const TaskKo ='[ ]'
+const TaskKo = '[ ]'
 
 //La lista de las tareas
-let TaskList =[]
+let TaskList = []
 
 
 /*DECLARACION DE FUNCIONES*/
 
 
 //Imprimir en pantalla las tareas
-function printTaskList (feedback){
-    function hayFeedback(feedback){
-        if(feedback == false){
+function printTaskList(feedback) {
+    function hayFeedback(feedback) {
+        if (feedback == false) {
             askMaimMenu();
         }
     };
     console.clear();
-    if (TaskList.length == 0){
-        console.log ('-------------------------------');
-        console.log ('La lista de tareas esta vacia');
-        console.log ('-------------------------------');
+    if (TaskList.length == 0) {
+        console.log('-------------------------------');
+        console.log('La lista de tareas esta vacia');
+        console.log('-------------------------------');
         hayFeedback(feedback);
-    }else{
-        console.log ('-------------------------------');
-        console.log ('La lista de tareas actual es: ');
-        console.log ('-------------------------------');
-        for(i = 0; i<TaskList.length; i++){
-            if (TaskList[i].done){
-                console.log ((i + 1), TaskOK, TaskList[i].description);
+    } else {
+        console.log('-------------------------------');
+        console.log('La lista de tareas actual es: ');
+        console.log('-------------------------------');
+        for (i = 0; i < TaskList.length; i++) {
+            if (TaskList[i].done) {
+                console.log((i + 1), TaskOK, TaskList[i].description);
             } else {
                 console.log((i + 1), TaskKo, TaskList[i].description);
             }
         }
         hayFeedback(feedback);
     }
-    
+
 };
 
 //Añadir tarea (Se añade a base de datos)
-function addTask (TaskList, taskDescription){
-    TaskList.push ({done: false, description: taskDescription});
+function addTask(TaskList, taskDescription) {
+    TaskList.push({ done: false, description: taskDescription });
 };
 
 //Seguir añadiendo tareas
-function moreTask (){
-    rl.question ('¿Quieres añadir mas tareas? (s/n)', function(seguirAñadiendo) {
-        if (seguirAñadiendo === 's' || seguirAñadiendo === 'S'){
+function moreTask() {
+    rl.question('¿Quieres añadir mas tareas? (s/n)', function(seguirAñadiendo) {
+        if (seguirAñadiendo === 's' || seguirAñadiendo === 'S') {
             askForTasks(TaskList);
-        }else if (seguirAñadiendo === 'n' || seguirAñadiendo === 'N'){
+        } else if (seguirAñadiendo === 'n' || seguirAñadiendo === 'N') {
             MaimMenu();
-        }else{
+        } else {
             console.log('\n\nDisculpa, no has añadido un valor valido.\n-Selecciona (s) en caso de (si)\n-Selecciona (n) en caso de (no)');
             moreTask();
         }
@@ -68,25 +67,25 @@ function moreTask (){
 }
 
 //Añadir tarea con Readline
-function askForTasks(TaskList){
+function askForTasks(TaskList) {
     console.clear();
-    if (TaskList.length == 0){
-        console.log ('-------------------------------');
-        console.log ('La lista de tareas esta vacia');
-        console.log ('-------------------------------');
-    }else{
+    if (TaskList.length == 0) {
+        console.log('-------------------------------');
+        console.log('La lista de tareas esta vacia');
+        console.log('-------------------------------');
+    } else {
         printTaskList(true);
     }
-    console.log ('Para volver al menu principal escribe back');
+    console.log('Para volver al menu principal escribe back');
     rl.question('Añade la nueva tarea: ', function(taskdesc) {
-        if (taskdesc === 'back'){
+        if (taskdesc === 'back') {
             MaimMenu();
-        }else{
+        } else {
             addTask(TaskList, taskdesc);
             console.log('La lista de tareas actual es: ');
             printTaskList(true);
             moreTask();
-        }       
+        }
     });
 };
 
@@ -94,13 +93,13 @@ function askForTasks(TaskList){
 function checker() {
     printTaskList();
     console.log("Back para volver");
-    rl.question ("Selecciona la tarea que quieres marcar como realizada: ", function (index) {
-        if (index >= 1 && index < (TaskList.length + 1) ){
+    rl.question("Selecciona la tarea que quieres marcar como realizada: ", function(index) {
+        if (index >= 1 && index < (TaskList.length + 1)) {
             TaskList[(index - 1)].done = true;
             checker();
-        }else if(index == 'back'){
+        } else if (index == 'back') {
             MaimMenu();
-        }else{
+        } else {
             console.log("El numero que indicas no es valido");
             checker();
         }
@@ -108,36 +107,37 @@ function checker() {
 };
 
 //Sistema de exit
-function exitting(){
+function exitting() {
     console.clear();
-    rl.question('¿Seguro de que quieres salir? (s/n): ', function (sureExit){
-        if (sureExit === 's' || sureExit === 'S'){
+    rl.question('¿Seguro de que quieres salir? (s/n): ', function(sureExit) {
+        if (sureExit === 's' || sureExit === 'S') {
             rl.close();
             console.clear();
             console.log('\nMuchas gracias por probar mi programa, sé que es muy feo pero lo he hecho con todo mi corazón. \n\n:)\n\n');
-        } else if (sureExit === 'n' || sureExit === 'N'){
+        } else if (sureExit === 'n' || sureExit === 'N') {
             MaimMenu();
         } else {
-            console.log ('\nDisculpa, no has añadido un valor valido.\n-Selecciona (s) en caso de (si)\n-Selecciona (n) en caso de (no)\n');
+            console.log('\nDisculpa, no has añadido un valor valido.\n-Selecciona (s) en caso de (si)\n-Selecciona (n) en caso de (no)\n');
             exitting();
         };
-    });    
+    });
 };
 
 //Borrar tareas realizadas
-function clearCheckeds(){
+function clearCheckeds() {
     printTaskList();
-    rl.question('¿Seguro de borrar las tareas realizadas? (s/n)', function (respuesta){
-        if (respuesta === 's'|| respuesta === 'S'){
-            for (i = 0; i < TaskList.length; i++){
-                if (TaskList[i].done){
+    rl.question('¿Seguro de borrar las tareas realizadas? (s/n)', function(respuesta) {
+        if (respuesta === 's' || respuesta === 'S') {
+            for (i = 0; i < TaskList.length; i++) {
+                if (TaskList[i].done) {
                     TaskList.splice(i, 1);
-                    i --;
+                    i--;
                 }
-            }MaimMenu();
-        }else if (respuesta === 'n' || respuesta === 'N'){
+            }
             MaimMenu();
-        }else{
+        } else if (respuesta === 'n' || respuesta === 'N') {
+            MaimMenu();
+        } else {
             ('\n\nDisculpa, no has añadido un valor valido.\n-Selecciona (s) en caso de (si)\n-Selecciona (n) en caso de (no)');
             clearCheckeds(); // Esto hace como si no pasara nada, porque vuelves a donde estabas y te borra lo que tenias porque printtasklist lo borra todo
         }
@@ -148,31 +148,31 @@ function clearCheckeds(){
 function Starter() {
     console.clear();
     console.log('\nBienvenido BetaTasker, un programa desarrollado por Michael Lara, gracias a los cursos de mastermind.\n')
-    rl.question("Pulsa enter para empezar", function (PrimeraPregunta){
-        if(PrimeraPregunta !== 'patatapodridaqueflipas'){
+    rl.question("Pulsa enter para empezar", function(PrimeraPregunta) {
+        if (PrimeraPregunta !== 'patatapodridaqueflipas') {
             MaimMenu();
         }
     });
 };
 
 //Consulta de volver al menú
-function askMaimMenu(){
-    rl.question('Pulsa enter para volver al menu principal', function(Enter){
-        if (Enter !== 'patataasada'){
+function askMaimMenu() {
+    rl.question('Pulsa enter para volver al menu principal', function(Enter) {
+        if (Enter !== 'patataasada') {
             MaimMenu();
         }
     })
 }
 
 //Menú principal
-function MaimMenu (){
+function MaimMenu() {
     console.clear();
-    console.log ('---------------');
-    console.log ('MENU PRINCIPAL');
-    console.log ('---------------');
-    console.log ('1.Ver la lista de tareas\n2.Añadir nueva tarea\n3.Dale check a las tareas realizadas\n4.Limpiar tareas ralizadas\n5.Salir');
-    rl.question('Seleccione una opcion: ', function (modo){
-        switch(modo){
+    console.log('---------------');
+    console.log('MENU PRINCIPAL');
+    console.log('---------------');
+    console.log('1.Ver la lista de tareas\n2.Añadir nueva tarea\n3.Dale check a las tareas realizadas\n4.Limpiar tareas ralizadas\n5.Salir');
+    rl.question('Seleccione una opcion: ', function(modo) {
+        switch (modo) {
             case '1':
                 printTaskList(false);
                 break;
@@ -184,15 +184,15 @@ function MaimMenu (){
                 break;
             case '4':
                 clearCheckeds();
-                break;         
+                break;
             case '5':
-                exitting();           
+                exitting();
                 break;
             default:
                 console.log('\nLo siento, el numero que indicas no figura en el menú.');
                 MaimMenu();
         }
-    
+
     });
 };
 
